61dd3a7882f037383efca9f0909e52f6c54389a9
John Coene <jcoenep@gmail.com>
2017-12-18 00:47:32 +0100

    documentation

--- R/datamaps.R
+++ R/datamaps.R
@@ -12,6 +12,10 @@
 #'   string and have \code{'px'} appended.
 #' @param elementId DOM id.
 #'
+#' @examples
+#' datamaps(projection = "orthographic") %>%
+#'   add_graticule()
+#'
 #' @import htmlwidgets
 #' @importFrom grDevices colorRampPalette
 #'
@@ -22,8 +26,6 @@ datamaps <- function(data, scope = "world", default = "#ABDDA4", projection = "e
 
   if(!missing(data))
     assign("data", data, envir = data_env)
-  else
-    stop("missing data", call. = FALSE)
 
   if(!tolower(scope) %in% c("usa", "world")) stop("incorrect scope, see details", call. = FALSE)
 
@@ -63,6 +65,8 @@ datamaps <- function(data, scope = "world", default = "#ABDDA4", projection = "e
 #' @param env The environment in which to evaluate \code{expr}.
 #' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
 #'   is useful if you want to save an expression in a variable.
+#' @param id id of map.
+#' @param session shiny session.
 #'
 #' @name datamaps-shiny
 #'
